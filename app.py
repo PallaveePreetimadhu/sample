a80052db1792dc33f169d917fa5fc07c754a2b47
PallaveePreetimadhu <taradattapradhana@MacBook-Air.local>
2025-05-06 12:32:20 +0530

    commit

--- app.py
+++ app.py
@@ -1,4 +1,33 @@
-a=12345
-b=234
+a=3
+b=8
 print(a+b)
-print("hello,everyone")
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
+
+
+a=5
+b=5.0
+print(a==b)
+print(a>=b)
+print(a!=b)
+
+a=5
+b=2
+print(a>b)
+print(a<b)
+
+a=5
+b=4
+print(a<=4)
+
+
+a=10
+c=-8
+d=[1,5,10,101]
+print(a in d)
+print(c in d)
+
